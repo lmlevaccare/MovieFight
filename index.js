d91bdd169fa0f6ff92508b8f4bdbe52aeb33c15f
lauren levaccare <laurenlevaccare@icloud.com>
2021-05-20 16:08:42 -0500

    mapping through object and appending data to page

--- index.js
+++ index.js
@@ -2,30 +2,25 @@ const fetchData = async (searchTerm) => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "750758ce",
-        s: searchTerm
+      s: searchTerm,
     },
   });
 
-  console.log(response.data);
+ return response.data.Search
 };
 
-//Debouncing the input event,so that search starts after user stops typing.
-const input = document.querySelector('input');
-let timeoutId;
-const onInput = (e) => {
-    if (timeoutId) {
-        clearTimeout(timeoutId)
-    }
-    timeoutId=setTimeout(() => {
-        fetchData(e.target.value);  
-    }, 3000)
-  
+//Debouncing the input event,so that search starts after user stops typing
+const input = document.querySelector("input");
+
+const onInput =  async event => {
+  const movies = await fetchData(event.target.value);
+  for (let movie of movies) {
+    const div = document.createElement('div');
+    div.innerHTML = `<img src="${movie.Poster}"/>
+    <h1>${movie.Title}`
+      ;
+    const target = document.getElementById('target')
+    target.appendChild(div)
+  }
 };
-
-input.addEventListener('input',onInput)
-
-
-  
-
-console.log()
-
+input.addEventListener("input", debounce(onInput, 1000));
