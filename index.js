91680fafa07c1b9489b30837ceea585752a07dc4
lauren levaccare <laurenlevaccare@icloud.com>
2021-05-21 11:49:26 -0500

    adding drop down menu

--- index.js
+++ index.js
@@ -5,22 +5,47 @@ const fetchData = async (searchTerm) => {
       s: searchTerm,
     },
   });
-
+  console.log("response", response)
+  if (response.data.Error) {
+    return []
+  }
  return response.data.Search
 };
 
 //Debouncing the input event,so that search starts after user stops typing
-const input = document.querySelector("input");
 
-const onInput =  async event => {
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+  <label><b>Search For a Movie</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`;
+
+const input = document.querySelector(".input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
+
+const onInput = async (event) => {
   const movies = await fetchData(event.target.value);
+  
+  resultsWrapper.innerHTML = "";
+  dropdown.classList.add('is-active');
   for (let movie of movies) {
-    const div = document.createElement('div');
-    div.innerHTML = `<img src="${movie.Poster}"/>
-    <h1>${movie.Title}`
-      ;
-    const target = document.getElementById('target')
-    target.appendChild(div)
+    const options= document.createElement('a');
+    // if poster === na then (?) assign empty string if not assign poster
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+    
+    options.classList.add('dropdown-item');
+    options.innerHTML = `
+      <img src="${imgSrc}" />
+      ${movie.Title}
+    `;
+   document.querySelector(".results").appendChild(options);
+ 
   }
 };
-input.addEventListener("input", debounce(onInput, 1000));
+input.addEventListener("input", debounce(onInput, 500));
