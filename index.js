418d647f0b502ddc12cf3d99de4149aaa5b6d720
lauren levaccare <laurenlevaccare@icloud.com>
2021-05-21 13:04:56 -0500

    2nd axios api call added

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const fetchData = async (searchTerm) => {
       s: searchTerm,
     },
   });
-  console.log("response", response)
+  console.log("response", response);
   if (response.data.Error) {
-    return []
+    return [];
   }
- return response.data.Search
+  return response.data.Search;
 };
 
 //Debouncing the input event,so that search starts after user stops typing
@@ -31,21 +31,52 @@ const resultsWrapper = document.querySelector(".results");
 
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value);
-  
+  if (!movies.length) {
+    dropdown.classList.remove('is-active');
+    return;
+}
   resultsWrapper.innerHTML = "";
-  dropdown.classList.add('is-active');
+
+  dropdown.classList.add("is-active");
   for (let movie of movies) {
-    const options= document.createElement('a');
+    const options = document.createElement("a");
     // if poster === na then (?) assign empty string if not assign poster
-    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-    
-    options.classList.add('dropdown-item');
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+
+    options.classList.add("dropdown-item");
     options.innerHTML = `
       <img src="${imgSrc}" />
       ${movie.Title}
     `;
-   document.querySelector(".results").appendChild(options);
- 
+
+    options.addEventListener('click', () => {
+      dropdown.classList.remove('is-active')
+      input.value = `${movie.Title}`
+      onMovieSelect(movie)
+    })
+    document.querySelector(".results").appendChild(options);
+
   }
 };
 input.addEventListener("input", debounce(onInput, 500));
+
+document.addEventListener('click', event => {
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove('is-active')
+   
+  }
+
+});
+
+const onMovieSelect = async movie => {
+  console.log(movie)
+  const responses = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "750758ce",
+      i: movie.imdbID,
+    },
+  });
+  ;
+   console.log("new data", responses.data);
+};
+
